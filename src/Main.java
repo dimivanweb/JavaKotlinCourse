666edd4635ad2dcaf82c0ecbf2ef9b48a1eda08c
Dmitrii Ivanov <dimivanweb@outlook.com>
2025-04-19 17:40:15 +0300

    Entry point and comment

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//публичный метод
+public class Main {
+    //точка входа в программу
+    public static void main(String[] args) {
+        /*
+        Выводит мое имя
+         */
+
+
+        System.out.println("Dmitrii Ivanov");
+
+
+
+    }
+}
